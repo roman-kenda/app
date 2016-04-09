c6204c0641cbf064a16f9b8cae0b9d874762b9bc
roman-kenda <sir.kenda@yandex.ru>
2016-04-09 20:51:57 +0300

    Add Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,33 @@
 class Product < ActiveRecord::Base
-	
+
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_then_or_equal_to: 0.01}
     validates :title, uniqueness: true
     validates :image_url, allow_blank: true, format:{
-    	with: %r{\.(gif|jpg|png)\Z}i,
-    	message: 'URL должен указывать на изображения в формата GIF, JPG или PNG'
+        with: %r{\.(gif|jpg|png)\Z}i,
+        message: 'URL должен указывать на изображения в формата GIF, JPG или PNG'
     }
     validates :title, allow_blank: true, length: {minimum: 10}
 
     def self.latest
         Product.order(:updated_at).last 
+    end
+
+    private 
+
+    #убеждаемся в отсутствии товарных позиций, ссылающийхся на данный товар
+
+    def ensure_not_referenced_by_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'cуществуют товарные позиции')
+            return false
+        end
     end
 
 end
