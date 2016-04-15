406184adebca7bcb6ae3c6eaa1b7b9b42fed2792
roman-kenda <sir.kenda@yandex.ru>
2016-04-15 11:30:17 +0300

    Order and RSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items do
     member do
       put 'decrement' 
@@ -8,7 +9,9 @@ Rails.application.routes.draw do
   resources :carts
   get 'store/index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member 
+  end
 
   root to: 'store#index', as: 'store'
   # The priority is based upon order of creation: first created -> highest priority.
